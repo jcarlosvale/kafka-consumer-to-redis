2b93028bdbfd4de8f6122f085831f6eb9d88b8f4
SOUS021 <joao.vale@arvato.com>
2020-09-27 20:29:06 +0300

    redis implementation

--- src/main/java/test/cybercube/consumer/Application.java
+++ src/main/java/test/cybercube/consumer/Application.java
@@ -2,13 +2,8 @@ package test.cybercube.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.kafka.annotation.EnableKafka;
-import test.cybercube.consumer.configuration.ConsumerProperties;
 
 @SpringBootApplication
-@EnableKafka
-@EnableConfigurationProperties(ConsumerProperties.class)
 public class Application {
 
     public static void main(String[] args) {
